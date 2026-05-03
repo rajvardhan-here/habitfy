118fd742425f1b9ed8f961c5ce26b31fee56a0e3
rajvardhan_here <rajvardhansinghchouhan1@gmail.com>
2026-05-03 18:10:39 +0530

    update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://dshtscrgfqymyfhuuplp.supabase.co";
-const supabaseAnonKey = "YOUR_ANON_PUBLIC_KEY";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
